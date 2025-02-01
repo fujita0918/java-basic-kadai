33d20d567ba09419ab7089db334a6c64e31c2b1a
fujita0918 <dreamfrog813@gmail.com>
2025-02-01 10:19:42 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		KatoTaro_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter KatoHanako = new KatoHanako_Chapter();
+		
+		KatoTaro.execIntroduce();
+		KatoIchiro.execIntroduce();
+		KatoHanako.execIntroduce();
+		
+	}
+
+}
---
+++ kadai_018/KatoHanako_Chapter.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoHanako_Chapter extends Kato_Chapter18 {
+
+	public  KatoHanako_Chapter() {
+	
+	 this.givenName = "花子";
+	 
+	}
+	 
+	 public void eachIntroduce() {
+		 System.out.println("趣味は読書です");
+	 }
+}
