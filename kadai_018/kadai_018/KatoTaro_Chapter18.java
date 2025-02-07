524803cc9af64e54e724ad8ca4f5c75887fea8c7
fujita0918 <dreamfrog813@gmail.com>
2025-02-07 22:19:47 +0900

    Add files via upload

---
+++ kadai_018/kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+	
+		// TODO 自動生成されたコンストラクター・スタブ
+	
+
+	public KatoTaro_Chapter18(){
+
+	 this.givenName = "太郎";
+	 
+	}
+	 public void eachIntroduce() {
+		 System.out.println("私はJavaが得意です");
+	 }
+}
